1e9970e5dc2f4e32ca27c5c746385dae039e4251
田口凌 <t20cs025@yamanashi.ac.jp>
2022-11-28 15:39:57 +0900

    [change]urls

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -5,4 +5,5 @@ from .views import ItemList
 app_name = 'shoppinglist'
 urlpatterns = [
     path('list/',ItemList.as_view(),name='list'),
+    path('edit', ItemEditView.as_view(),name='edit'),
     ]
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -20,3 +20,27 @@ class ItemList(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = ItemBuy()
         return context
+    
+    class ItemEditView(TemplateView):
+        model = Item
+        template_name = 'shoppinglist/item_edit.html'
+        success_url = 'list/'
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        name = self.request.POST.get('name')
+        count = self.request.POST.get('count')
+        buy_date = self.request.POST.get('buy_date')
+
+        item = get_object_or_404(Item, pk=item_id)
+        item.name = name
+        item.count = count
+        item.buy_date = buy_date
+        item.save()
+        return HttpResponseRedirect(reverse('shoppinglist:list'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        context['form'] = ItemForm()
+        return context
