aa8b55b5b9015d56ed561bc9d96f8d3431d02895
田口凌 <t20cs025@yamanashi.ac.jp>
2022-11-28 16:52:24 +0900

    [add]edit completely

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ItemList
+from .views import ItemList, ItemEditView
 
 
 app_name = 'shoppinglist'
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -3,7 +3,9 @@ from .models import Item
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
-from .forms import ItemBuy
+from .forms import ItemBuy, ItemIdForm, ItemForm
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
 class ItemList(ListView):
     model = Item
@@ -21,26 +23,27 @@ class ItemList(ListView):
         context['form'] = ItemBuy()
         return context
     
-    class ItemEditView(TemplateView):
+class ItemEditView(TemplateView):
         model = Item
         template_name = 'shoppinglist/item_edit.html'
         success_url = 'list/'
 
-    def post(self, request, *args, **kwargs):
-        item_id = self.request.POST.get('item_id')
-        name = self.request.POST.get('name')
-        count = self.request.POST.get('count')
-        buy_date = self.request.POST.get('buy_date')
+        def post(self, request, *args, **kwargs):
+            item_id = self.request.POST.get('item_id')
+            name = self.request.POST.get('name')
+            count = self.request.POST.get('count')
+            buy_date = self.request.POST.get('buy_date')
 
-        item = get_object_or_404(Item, pk=item_id)
-        item.name = name
-        item.count = count
-        item.buy_date = buy_date
-        item.save()
-        return HttpResponseRedirect(reverse('shoppinglist:list'))
+            item = get_object_or_404(Item, pk=item_id)
+            item.name = name
+            item.count = count
+            item.buy_date = buy_date
+            item.save()
+            return HttpResponseRedirect(reverse('shoppinglist:list'))
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = ItemIdForm()
-        context['form'] = ItemForm()
-        return context
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['form_id'] = ItemIdForm()
+            context['form'] = ItemForm()
+            return context
+        
