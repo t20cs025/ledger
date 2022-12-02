1692d883ae2d791c72020300308c3da2f6349590
Reo Baba <t20cs038@yamanashi.ac.jp>
2022-12-02 13:41:22 +0900

    [add] viewにItemShowViewを追加,urlsにpathを追加

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -6,4 +6,5 @@ app_name = 'shoppinglist'
 urlpatterns = [
     path('list/',ItemList.as_view(),name='list'),
     path('edit', ItemEditView.as_view(),name='edit'),
+    path('show', ItemShowView.as_view(), name='show'),
     ]
